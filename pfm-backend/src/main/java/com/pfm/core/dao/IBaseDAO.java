616b1a7033e88e8e7e78773b7fa5d117e9a566c7
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-10-15 22:20:02 -0300

    Organize Packages

---
+++ pfm-backend/src/main/java/com/pfm/core/dao/IBaseDAO.java
@@ -0,0 +1,21 @@
+package com.pfm.core.dao;
+
+import java.util.List;
+
+public interface IBaseDAO<T> {
+
+	T save(T model);
+	
+	T paginatedSearch(T model);
+	
+	List<T> search(T model);
+	
+	T update(T model);
+
+	Boolean deleteById(Integer id);
+
+	T getById(Integer id);
+
+	List<T> findAll();
+	
+}
---
+++ pfm-backend/src/main/java/com/pfm/transaction/enums/TransactionCategory.java
@@ -0,0 +1,21 @@
+package com.pfm.transaction.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum TransactionCategory {
+	
+	FOOD, 
+	
+	TRANSPORT,
+	
+	SALARY,
+	
+	ENTERTAINMENT,
+	
+	HEALTH,
+	
+	OTHER;
+}
